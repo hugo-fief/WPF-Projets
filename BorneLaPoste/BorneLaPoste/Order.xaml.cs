e7c9235aa5e6f0b73060a1cc0e6a5c8f784fdb20
HugoWinterGhost <hfief1806@gmail.com>
2024-01-18 17:53:11 +0100

    adding order & completing v1 finished

--- BorneLaPoste/BorneLaPoste/Order.xaml.cs
+++ BorneLaPoste/BorneLaPoste/Order.xaml.cs
@@ -13,6 +13,17 @@ namespace BorneLaPoste
         public Order(OrderData updatingOrderData)
         {
             InitializeComponent();
+            InitOrder(updatingOrderData);
+        }
+
+        /// <summary>
+        /// Init the order
+        /// </summary>
+        /// <param name="orderData"></param>
+        private void InitOrder(OrderData updatingOrderData)
+        {
+            basketName.Content = updatingOrderData.Name;
+            basketPrice.Content = updatingOrderData.Price;
             OrderData = updatingOrderData;
         }
 
@@ -24,6 +35,42 @@ namespace BorneLaPoste
         private void ReturnSuiviWindow(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        /// <summary>
+        /// Increase the quantity of the order
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void IncreaseQuantity(object sender, RoutedEventArgs e)
+        {
+            string currentQuantity = basketQuantity.Text;
+            int updatedQuantity = int.Parse(currentQuantity) + 1;
+            basketQuantity.Text = updatedQuantity.ToString();
+
+            basketPrice.Content = OrderData.Price * updatedQuantity;
+        }
+
+        /// <summary>
+        /// Increase the quantity of the order
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DecreaseQuantity(object sender, RoutedEventArgs e)
+        {
+            string currentQuantity = basketQuantity.Text;
+            int updatedQuantity = int.Parse(currentQuantity) - 1;
+            basketQuantity.Text = updatedQuantity.ToString();
+        }
+
+        /// <summary>
+        /// Open the card window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CardOpen(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Payment effectué. Bonne journée à vous !", "Message de succès", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
